8ea1d9b041dc9d87608fb6c6b88f13f7a88cc88e
Jan Schejbal <schejbal.jan2@gmail.com>
2025-10-09 11:01:49 +0200

    second activity

--- MyApp004MoreActivities/app/src/main/java/com/example/myapp004moreactivities/MainActivity.kt
+++ MyApp004MoreActivities/app/src/main/java/com/example/myapp004moreactivities/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.myapp004moreactivities
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextName: EditText
+    private lateinit var editTextMessage: EditText
+    private lateinit var buttonSend: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Inicializace views
+        editTextName = findViewById(R.id.editTextName)
+        editTextMessage = findViewById(R.id.editTextMessage)
+        buttonSend = findViewById(R.id.buttonSend)
+
+        // Nastavení onClick listeneru pro tlačítko
+        buttonSend.setOnClickListener {
+            val name = editTextName.text.toString()
+            val message = editTextMessage.text.toString()
+
+            // Vytvoření intentu pro SecondActivity
+            val intent = Intent(this, SecondActivity::class.java)
+
+            // Přidání dat do intentu
+            intent.putExtra("EXTRA_NAME", name)
+            intent.putExtra("EXTRA_MESSAGE", message)
+
+            // Spuštění druhé aktivity
+            startActivity(intent)
         }
     }
 }
